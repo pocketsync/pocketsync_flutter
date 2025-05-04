b8ca1f525c49a200bac4044538a8cac6b4e92254
Steve NOSSE <nossesteve@gmail.com>
2025-05-04 12:03:25 +0200

    reduced debounce interval

--- lib/src/utils/sync_config.dart
+++ lib/src/utils/sync_config.dart
@@ -1,6 +1,6 @@
 class SyncConfig {
   /// Default debounce interval for sync operations.
-  static const Duration defaultDebounceInterval = Duration(seconds: 5);
+  static const Duration defaultDebounceInterval = Duration(seconds: 3);
 
   static const String defaultGlobalIdColumnName = 'ps_global_id';
 
