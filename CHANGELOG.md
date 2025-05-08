66ca0b9b83d250c2aef0ec5ed164a5517673e1a5
Steve NOSSE <nossesteve@gmail.com>
2025-05-09 00:04:46 +0200

    support in memory db

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## 0.4.0
 - Split changes into batches to prevent server overload
 - Properly handle connection and disconnection events (and avoid unnecessary syncs)
+- Add support for in memory database
 
 ## 0.3.0
 - Complete refactoring of the engine
--- lib/pocketsync_flutter.dart
+++ lib/pocketsync_flutter.dart
@@ -1,4 +1,4 @@
-export 'package:sqflite/sqflite.dart' show getDatabasesPath, ConflictAlgorithm;
+export 'package:sqflite/sqflite.dart' show getDatabasesPath, ConflictAlgorithm, inMemoryDatabasePath;
 export 'src/pocket_sync.dart';
 export 'src/database/pocket_sync_database.dart';
 export 'src/models/types.dart'
